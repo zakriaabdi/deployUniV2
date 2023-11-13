4c520a8ecd8d7b05e3362a318341c880b85786ef
Zak Abdi <zakaria.abdi@wintermute.com>
2023-11-13 09:58:44 +0000

    change env vars to args

--- deploy_uni_v2.py
+++ deploy_uni_v2.py
@@ -3,25 +3,30 @@ from web3.contract.contract import Contract
 from eth_account.signers.local import (LocalAccount)
 import json
 import os
+import argparse
 
- 
+parser = argparse.ArgumentParser(description="") 
+parser.add_argument('--private-key', type=str, help="Private key for deployer addr, beginning with 0x")
+parser.add_argument('--rpc-url',type=str, help="specify the rpc endpoint you are deploying to (https)")
+parser.add_argument('--governance-addr', type=str, help="specify the governance address to be used in constructor")
+
+args =  parser.parse_args()
+PRIVATE_KEY = args.private_key
+RPC_URL = args.rpc_url
+GOVERNANCE_ADDR = args.governance_addr
+
 # Set provider
-w3 = Web3(HTTPProvider('SET PROVIDER'))
+w3 = Web3(HTTPProvider(RPC_URL))
 
 
 # set env var
-DEPLOYER: LocalAccount = w3.eth.account.from_key(os.environ.get("PRIVATE_KEY"))
-# Set constructor param to right governance addr
-V2_OWNER = None
-
+DEPLOYER: LocalAccount = w3.eth.account.from_key(PRIVATE_KEY)
 abi = None
 bytecode = None
 
 
 gas_limit = 10_000_000
 nonce = w3.eth.get_transaction_count(DEPLOYER.address)
-
-
 # NOTE: Best to check with whichever network you are deploying to since some have weird gas systems (e.g. Boba network)
 gas_price = w3.eth.gas_price
 
@@ -35,7 +40,7 @@ with open("./canonical_artifacts/UniswapV2Factory.json", 'r') as artifact:
 UniV2Factory: Contract = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 
-tx = UniV2Factory.constructor(V2_OWNER).build_transaction({"from": DEPLOYER.address, "gasPrice": gas_price, 'gas': gas_limit, 'nonce': nonce})
+tx = UniV2Factory.constructor(GOVERNANCE_ADDR).build_transaction({"from": DEPLOYER.address, "gasPrice": gas_price, 'gas': gas_limit, 'nonce': nonce})
 singed_tx = DEPLOYER.sign_transaction(tx)
 
 res: bytes = w3.eth.send_raw_transaction(singed_tx.rawTransaction)
